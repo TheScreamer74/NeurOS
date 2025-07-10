4216e733b8bae4d99073bef6d9a986c57a188c93
Florian Chanson <fchanson@solem.fr>
2025-07-10 16:10:00 +0200

    run but encoding issue

--- STT-Test/TTS/tts_model.py
+++ STT-Test/TTS/tts_model.py
@@ -4,17 +4,18 @@ import json
 import base64
 import torch
 import numpy as np
-from chatterbox.tts import ChatterboxTTS
+from classes import Bicodec, Spark
 
 device = "mps" if torch.backends.mps.is_available() else "cpu"
 AUDIO_PROMPT_PATH = "sw3.wav"
 
 bicodec = Bicodec("BiCodec", "wav2vec2-ST")
-spark = Spark("model.q4_k.gguf")
-tokens, codes = bicodec.encode(input)
+spark = Spark("model.q8_0.gguf")
+tokens, codes = bicodec.encode(AUDIO_PROMPT_PATH)
 
 async def handle_ws():
-    uri = "ws://127.0.0.1:8080/models/connect"
+    global codes
+    uri = "ws://192.168.101.112:8080/models/connect"
 
     async with websockets.connect(uri) as websocket:
         await websocket.send(json.dumps({
@@ -34,9 +35,9 @@ async def handle_ws():
                     print(f"🔊 TTS request received: {text}")
 
                     codes = spark.generate(text, codes)
-                    audio_bytes = bicodec.decode(tokens, codes)
-
-                    # Encode base64
+                    
+                    audio_array = bicodec.decode(tokens, codes)
+                    audio_bytes = audio_array.astype(np.float32).tobytes()
                     audio_base64 = base64.b64encode(audio_bytes).decode("utf-8")
 
                     # Send raw 32-bit float PCM data
